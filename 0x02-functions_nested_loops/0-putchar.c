278d4532327430b81fe51490a8ece95e9319f45e
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2023-02-21 07:26:44 -0800

    asd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - prints _putchar followed by a new line
  *
- * Return: Always 0.
+ * Return: Always 0. (Success)
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
-#include<unistd.h>
 #include "main.h"
 
 /**
  * print_alphabet - prints in lowercase
  *
  * followed by a new line
+ *
+ * Return always 0 (Success)
  */
 void print_alphabet(void)
 {
-	char c;
+	char c = 'a';
 
-	c = 'a';
 	while (c <= 'z')
 	{
 		_putchar(c);
