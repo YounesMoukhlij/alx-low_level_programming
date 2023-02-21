5c19de835ee959e2c3d89ba0d8798b6e4066f7d4
YounesMoukhlij <younesmoukhlijofficial@gmail.com>
2023-02-21 07:15:01 -0800

    ss

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include<unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints in lowercase
+ *
+ * followed by a new line
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	while (c <= 'z')
+	{
+		_putchar(z);
+		c++;
+	}
+	_putchar('\n');
+}
